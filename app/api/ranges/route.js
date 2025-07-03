5d4bb87386de6ca1cd7d64f8a50a9fcc1088e098
VKapitan4 <vkapitano@pmfst.hr>
2025-07-03 13:01:17 +0200

    adding range issue fixed

--- app/api/ranges/route.js
+++ app/api/ranges/route.js
@@ -2,6 +2,7 @@
 
 import { NextResponse } from 'next/server';
 import clientPromise from '@/app/lib/db'; // ili '@/lib/mongodb'
+import { ObjectId } from 'mongodb'; // KLJUČAN IMPORT za rad s _id
 
 export async function GET() {
     try {
